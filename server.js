b3276683a04a08f57c33c8ff18c3a3d448cbfa78
jamescr757 <55260759+jamescr757@users.noreply.github.com>
2019-12-12 16:20:10 -0600

    added a comment on server.js

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ mongoose.connect(MONGODB_URI);
 
 const teamsArray = ["bruins", "sabres", "red wings", "panthers", "canadiens", "senators", "lightning", "maple leafs", "hurricanes", "blue jackets", "devils", "islanders", "rangers", "flyers", "penguins", "capitals", "blackhawks", "avalanche", "stars", "wild", "predators", "blues", "jets", "ducks", "coyotes", "flames", "oilers", "golden knights", "kings", "sharks", "canucks"];
 
+// used to create unique id for each game so games aren't scraped twice
 function teamIDGenerator(teamName) {
     return teamsArray.indexOf(teamName);
 }
